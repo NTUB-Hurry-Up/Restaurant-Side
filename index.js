0fb888b02f47820c3c58ba704637505c30f0a1ba
joe890120 <joelin890120@gmail.com>
2019-04-02 17:20:39 +0800

    linebot108508

--- index.js
+++ index.js
@@ -260,8 +260,7 @@ bot.on('message', function (event) {
                     states="進入修改電話程序";
                     event.reply('請輸入您的電話\nex: 09xxxxxxxx');                    
                 }
-
-            }else if(msg1 == "店家"){
+            }else if(msg=="店家"){
                 event.reply("hi");       
             }else if(states != ""){
                 if(states=="進入修改電話程序"){
