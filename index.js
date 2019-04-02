94a6a8b5ad107b4bff81469060a424b6adb99c12
joe890120 <joelin890120@gmail.com>
2019-04-02 17:34:08 +0800

    linebot108508

--- index.js
+++ index.js
@@ -36,184 +36,6 @@ bot.on('follow', function (event) {
 // --------------------------------
 // 機器人接受訊息的處理
 // --------------------------------
-var temp1=
-{
-    "type": "template",
-    "altText": "this is a buttons template",
-    "template": {
-        "type": "buttons",
-        "actions": [
-            {
-            "type": "message",
-            "label": "看到的字",
-            "text": "傳出的字"
-            },
-            {
-            "type": "message",
-            "label": "看到的字",
-            "text": "傳出的字"
-            }
-        ],
-        "title": "標題",
-        "text": "內文"
-    }
-}
-var temp_store=
- {
-    "type": "flex",
-    "altText": "Flex Message",
-    "contents": {
-        "type": "bubble",
-        "hero": {
-        "type": "image",
-        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-        "size": "full",
-        "aspectRatio": "20:13",
-        "aspectMode": "cover",
-        "action": {
-            "type": "uri",
-            "label": "Line",
-            "uri": "https://linecorp.com/"
-        }
-        },
-        "body": {
-        "type": "box",
-        "layout": "vertical",
-        "contents": [
-            {
-            "type": "text",
-            "text": "Brown Cafe",
-            "size": "xl",
-            "weight": "bold"
-            },
-            {
-            "type": "box",
-            "layout": "baseline",
-            "margin": "md",
-            "contents": [
-                {
-                "type": "icon",
-                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
-                "size": "sm"
-                },
-                {
-                "type": "icon",
-                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
-                "size": "sm"
-                },
-                {
-                "type": "icon",
-                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
-                "size": "sm"
-                },
-                {
-                "type": "icon",
-                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
-                "size": "sm"
-                },
-                {
-                "type": "icon",
-                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png",
-                "size": "sm"
-                },
-                {
-                "type": "text",
-                "text": "4.0",
-                "flex": 0,
-                "margin": "md",
-                "size": "sm",
-                "color": "#999999"
-                }
-            ]
-            },
-            {
-            "type": "box",
-            "layout": "vertical",
-            "spacing": "sm",
-            "margin": "lg",
-            "contents": [
-                {
-                "type": "box",
-                "layout": "baseline",
-                "spacing": "sm",
-                "contents": [
-                    {
-                    "type": "text",
-                    "text": "Place",
-                    "flex": 1,
-                    "size": "sm",
-                    "color": "#AAAAAA"
-                    },
-                    {
-                    "type": "text",
-                    "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
-                    "flex": 5,
-                    "size": "sm",
-                    "color": "#666666",
-                    "wrap": true
-                    }
-                ]
-                },
-                {
-                "type": "box",
-                "layout": "baseline",
-                "spacing": "sm",
-                "contents": [
-                    {
-                    "type": "text",
-                    "text": "Time",
-                    "flex": 1,
-                    "size": "sm",
-                    "color": "#AAAAAA"
-                    },
-                    {
-                    "type": "text",
-                    "text": "10:00 - 23:00",
-                    "flex": 5,
-                    "size": "sm",
-                    "color": "#666666",
-                    "wrap": true
-                    }
-                ]
-                }
-            ]
-            }
-        ]
-        },
-        "footer": {
-        "type": "box",
-        "layout": "vertical",
-        "flex": 0,
-        "spacing": "sm",
-        "contents": [
-            {
-            "type": "button",
-            "action": {
-                "type": "uri",
-                "label": "CALL",
-                "uri": "https://linecorp.com"
-            },
-            "height": "sm",
-            "style": "link"
-            },
-            {
-            "type": "button",
-            "action": {
-                "type": "uri",
-                "label": "WEBSITE",
-                "uri": "https://linecorp.com"
-            },
-            "height": "sm",
-            "style": "link"
-            },
-            {
-            "type": "spacer",
-            "size": "sm"
-            }
-        ]
-        }
-    }
-}
 
 var states = "";
 
@@ -247,7 +69,7 @@ bot.on('message', function (event) {
                             temp1.template.actions[1].text="會員,修改電話";
                             temp1.template.title="會員資訊"
                             temp1.template.text="姓名 : "+data.name+"\n電話 : "+data.phone
-                            event.reply(temp1);
+                            event.reply(temp.temp1);
                         }
                     })
                 }else if(msg2=="修改姓名"){
@@ -259,7 +81,7 @@ bot.on('message', function (event) {
                     event.reply('請輸入您的電話\nex: 09xxxxxxxx');                    
                 }
             }else if(msg1=="店家"){
-                event.reply(temp_store);
+                // event.reply(temp_store);
             }else if(states != ""){
                 if(states=="進入修改電話程序"){
                     states="";
