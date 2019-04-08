43df2b6416000a481f60451f83624e319f0c6153
joe890120 <joelin890120@gmail.com>
2019-04-08 13:14:55 +0800

    linebot108508

--- index.js
+++ index.js
@@ -211,10 +211,10 @@ bot.on('message', function (event) {
                             for(var i = 0; i<data.length; i++){
                             //     // console.log(i);
                                 // console.log(data[i].storeName, data[i].storeAdd, data[i].storeTel);
-                                o.body.contents[0].text=data[i].storeName;
-                                o.body.contents[1].contents[0].contents[1].text=data[i].storeAdd;
-                                o.body.contents[1].contents[1].contents[1].text=data[i].storeTel;
                                 arr[0].contents.contents.push(o);
+                                arr[0].contents.contents[i].body.contents[0].text=data[i].storeName;
+                                arr[0].contents.contents[i].body.contents[1].contents[0].contents[1].text=data[i].storeAdd;
+                                arr[0].contents.contents[i].body.contents[1].contents[1].contents[1].text=data[i].storeTel;
                             }
                             // data.forEach(m => {
                                 // console.log(m.storeName, m.storeAdd, m.storeTel);
