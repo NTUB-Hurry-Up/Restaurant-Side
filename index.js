0637493b98cb2b522e93dfa14c81a668313de2c9
joe890120 <joelin890120@gmail.com>
2019-04-08 11:18:26 +0800

    linebot108508

--- index.js
+++ index.js
@@ -87,131 +87,172 @@ bot.on('message', function (event) {
                 if(msg2=="資訊"){
                     store.fetchStore().then(data => {
                         var arr=[];
-                        var o = {
-                            "type": "bubble",
-                            "hero": {
-                              "type": "image",
-                              "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-                              "size": "full",
-                              "aspectRatio": "20:13",
-                              "aspectMode": "cover",
-                              "action": {
-                                "type": "uri",
-                                "label": "Line",
-                                "uri": "https://linecorp.com/"
-                              }
-                            },
-                            "body": {
-                              "type": "box",
-                              "layout": "vertical",
-                              "contents": [
-                                {
-                                  "type": "text",
-                                  "text": "storeName",
-                                  "size": "xl",
-                                  "weight": "bold"
-                                },
-                                {
-                                  "type": "box",
-                                  "layout": "vertical",
-                                  "spacing": "sm",
-                                  "margin": "lg",
-                                  "contents": [
-                                    {
-                                      "type": "box",
-                                      "layout": "baseline",
-                                      "spacing": "sm",
-                                      "contents": [
-                                        {
-                                          "type": "text",
-                                          "text": "Place",
-                                          "flex": 1,
-                                          "size": "sm",
-                                          "color": "#AAAAAA"
-                                        },
-                                        {
-                                          "type": "text",
-                                          "text": "storeAdd",
-                                          "flex": 5,
-                                          "size": "sm",
-                                          "color": "#666666",
-                                          "wrap": true
-                                        }
-                                      ]
-                                    },
-                                    {
-                                      "type": "box",
-                                      "layout": "baseline",
-                                      "spacing": "sm",
-                                      "contents": [
-                                        {
-                                          "type": "text",
-                                          "text": "Tel",
-                                          "flex": 1,
-                                          "size": "sm",
-                                          "color": "#AAAAAA"
-                                        },
-                                        {
-                                          "type": "text",
-                                          "text": "storeTel",
-                                          "flex": 5,
-                                          "size": "sm",
-                                          "color": "#666666",
-                                          "wrap": true
-                                        }
-                                      ]
-                                    }
-                                  ]
-                                }
-                              ]
-                            },
-                            "footer": {
-                              "type": "box",
-                              "layout": "vertical",
-                              "flex": 0,
-                              "spacing": "sm",
-                              "contents": [
-                                {
-                                  "type": "button",
-                                  "action": {
-                                    "type": "uri",
-                                    "label": "CALL",
-                                    "uri": "https://linecorp.com"
-                                  },
-                                  "height": "sm",
-                                  "style": "link"
-                                },
-                                {
-                                  "type": "button",
-                                  "action": {
-                                    "type": "uri",
-                                    "label": "WEBSITE",
-                                    "uri": "https://linecorp.com"
-                                  },
-                                  "height": "sm",
-                                  "style": "link"
-                                },
-                                {
-                                  "type": "spacer",
-                                  "size": "sm"
-                                }
-                              ]
+                        var o = 
+                        {
+                          "type": "bubble",
+                          "hero": {
+                            "type": "image",
+                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+                            "size": "full",
+                            "aspectRatio": "20:13",
+                            "aspectMode": "cover",
+                            "action": {
+                              "type": "uri",
+                              "label": "Line",
+                              "uri": "https://linecorp.com/"
                             }
+                          },
+                          "body": {
+                            "type": "box",
+                            "layout": "vertical",
+                            "contents": [
+                              {
+                                "type": "text",
+                                "text": "Brown Cafe",
+                                "size": "xl",
+                                "weight": "bold"
+                              },
+                              {
+                                "type": "box",
+                                "layout": "baseline",
+                                "margin": "md",
+                                "contents": [
+                                  {
+                                    "type": "icon",
+                                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+                                    "size": "sm"
+                                  },
+                                  {
+                                    "type": "icon",
+                                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+                                    "size": "sm"
+                                  },
+                                  {
+                                    "type": "icon",
+                                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+                                    "size": "sm"
+                                  },
+                                  {
+                                    "type": "icon",
+                                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+                                    "size": "sm"
+                                  },
+                                  {
+                                    "type": "icon",
+                                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png",
+                                    "size": "sm"
+                                  },
+                                  {
+                                    "type": "text",
+                                    "text": "4.0",
+                                    "flex": 0,
+                                    "margin": "md",
+                                    "size": "sm",
+                                    "color": "#999999"
+                                  }
+                                ]
+                              },
+                              {
+                                "type": "box",
+                                "layout": "vertical",
+                                "spacing": "sm",
+                                "margin": "lg",
+                                "contents": [
+                                  {
+                                    "type": "box",
+                                    "layout": "baseline",
+                                    "spacing": "sm",
+                                    "contents": [
+                                      {
+                                        "type": "text",
+                                        "text": "Place",
+                                        "flex": 1,
+                                        "size": "sm",
+                                        "color": "#AAAAAA"
+                                      },
+                                      {
+                                        "type": "text",
+                                        "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
+                                        "flex": 5,
+                                        "size": "sm",
+                                        "color": "#666666",
+                                        "wrap": true
+                                      }
+                                    ]
+                                  },
+                                  {
+                                    "type": "box",
+                                    "layout": "baseline",
+                                    "spacing": "sm",
+                                    "contents": [
+                                      {
+                                        "type": "text",
+                                        "text": "Time",
+                                        "flex": 1,
+                                        "size": "sm",
+                                        "color": "#AAAAAA"
+                                      },
+                                      {
+                                        "type": "text",
+                                        "text": "10:00 - 23:00",
+                                        "flex": 5,
+                                        "size": "sm",
+                                        "color": "#666666",
+                                        "wrap": true
+                                      }
+                                    ]
+                                  }
+                                ]
+                              }
+                            ]
+                          },
+                          "footer": {
+                            "type": "box",
+                            "layout": "vertical",
+                            "flex": 0,
+                            "spacing": "sm",
+                            "contents": [
+                              {
+                                "type": "button",
+                                "action": {
+                                  "type": "uri",
+                                  "label": "CALL",
+                                  "uri": "https://linecorp.com"
+                                },
+                                "height": "sm",
+                                "style": "link"
+                              },
+                              {
+                                "type": "button",
+                                "action": {
+                                  "type": "uri",
+                                  "label": "WEBSITE",
+                                  "uri": "https://linecorp.com"
+                                },
+                                "height": "sm",
+                                "style": "link"
+                              },
+                              {
+                                "type": "spacer",
+                                "size": "sm"
+                              }
+                            ]
                           }
+                        };
                         if (data == -1){
                             event.reply('找不到資料');
                         }else if(data == -9){                    
                             event.reply('執行錯誤');
                         }else{
-                            arr.push(temp.temp_store);
-                            arr.push(o);
+                            arr.push(temp.temp_store)
+                            arr.temp.temp_store.contents.contents.push(o);
                             // data.forEach(m => {
                                 // console.log(m.storeid);
                                 // o.body.contents[0].text=m.storeName;
                                 // o.body.contents[1].contents[0].contents[1].text=m.storeAdd;
                                 // o.body.contents[1].contents[1].contents[1].text=m.storeTel;
                                 // console.log("wertyu"+m);
-                                // console.log();
+                                // console.log();;
                             // })
                         }
                         event.reply(arr);
