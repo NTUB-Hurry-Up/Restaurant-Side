a1b423252dc91079b13d075689cf5d4a5ff2d5a2
joe890120 <joelin890120@gmail.com>
2019-04-08 12:45:56 +0800

    linebot108508

--- index.js
+++ index.js
@@ -84,121 +84,121 @@ bot.on('message', function (event) {
                     event.reply('請輸入您的電話\nex: 09xxxxxxxx');                    
                 }
             }else if(msg1=="店家"){
-                if(msg2=="資訊"){
-                    store.fetchStore().then(data => {
-                        var arr=[];
-                        var o = 
-                        {
-                          "type": "bubble",
-                          "hero": {
-                            "type": "image",
-                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-                            "size": "full",
-                            "aspectRatio": "20:13",
-                            "aspectMode": "cover",
-                            "action": {
-                              "type": "uri",
-                              "label": "Line",
-                              "uri": "https://linecorp.com/"
-                            }
+                if(msg2=="資訊"){var arr=[];
+                    var o = 
+                    {
+                      "type": "bubble",
+                      "hero": {
+                        "type": "image",
+                        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+                        "size": "full",
+                        "aspectRatio": "20:13",
+                        "aspectMode": "cover",
+                        "action": {
+                          "type": "uri",
+                          "label": "Line",
+                          "uri": "https://linecorp.com/"
+                        }
+                      },
+                      "body": {
+                        "type": "box",
+                        "layout": "vertical",
+                        "contents": [
+                          {
+                            "type": "text",
+                            "text": "Brown Cafe",
+                            "size": "xl",
+                            "weight": "bold"
                           },
-                          "body": {
+                          {
                             "type": "box",
                             "layout": "vertical",
+                            "spacing": "sm",
+                            "margin": "lg",
                             "contents": [
                               {
-                                "type": "text",
-                                "text": "Brown Cafe",
-                                "size": "xl",
-                                "weight": "bold"
+                                "type": "box",
+                                "layout": "baseline",
+                                "spacing": "sm",
+                                "contents": [
+                                  {
+                                    "type": "text",
+                                    "text": "Place",
+                                    "flex": 1,
+                                    "size": "sm",
+                                    "color": "#AAAAAA"
+                                  },
+                                  {
+                                    "type": "text",
+                                    "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
+                                    "flex": 5,
+                                    "size": "sm",
+                                    "color": "#666666",
+                                    "wrap": true
+                                  }
+                                ]
                               },
                               {
                                 "type": "box",
-                                "layout": "vertical",
+                                "layout": "baseline",
                                 "spacing": "sm",
-                                "margin": "lg",
                                 "contents": [
                                   {
-                                    "type": "box",
-                                    "layout": "baseline",
-                                    "spacing": "sm",
-                                    "contents": [
-                                      {
-                                        "type": "text",
-                                        "text": "Place",
-                                        "flex": 1,
-                                        "size": "sm",
-                                        "color": "#AAAAAA"
-                                      },
-                                      {
-                                        "type": "text",
-                                        "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
-                                        "flex": 5,
-                                        "size": "sm",
-                                        "color": "#666666",
-                                        "wrap": true
-                                      }
-                                    ]
+                                    "type": "text",
+                                    "text": "Time",
+                                    "flex": 1,
+                                    "size": "sm",
+                                    "color": "#AAAAAA"
                                   },
                                   {
-                                    "type": "box",
-                                    "layout": "baseline",
-                                    "spacing": "sm",
-                                    "contents": [
-                                      {
-                                        "type": "text",
-                                        "text": "Time",
-                                        "flex": 1,
-                                        "size": "sm",
-                                        "color": "#AAAAAA"
-                                      },
-                                      {
-                                        "type": "text",
-                                        "text": "10:00 - 23:00",
-                                        "flex": 5,
-                                        "size": "sm",
-                                        "color": "#666666",
-                                        "wrap": true
-                                      }
-                                    ]
+                                    "type": "text",
+                                    "text": "10:00 - 23:00",
+                                    "flex": 5,
+                                    "size": "sm",
+                                    "color": "#666666",
+                                    "wrap": true
                                   }
                                 ]
                               }
                             ]
-                          },
-                          "footer": {
-                            "type": "box",
-                            "layout": "vertical",
-                            "flex": 0,
-                            "spacing": "sm",
-                            "contents": [
-                              {
-                                "type": "button",
-                                "action": {
-                                  "type": "uri",
-                                  "label": "CALL",
-                                  "uri": "https://linecorp.com"
-                                },
-                                "height": "sm",
-                                "style": "link"
-                              },
-                              {
-                                "type": "button",
-                                "action": {
-                                  "type": "uri",
-                                  "label": "WEBSITE",
-                                  "uri": "https://linecorp.com"
-                                },
-                                "height": "sm",
-                                "style": "link"
-                              },
-                              {
-                                "type": "spacer",
-                                "size": "sm"
-                              }
-                            ]
                           }
-                        };
+                        ]
+                      },
+                      "footer": {
+                        "type": "box",
+                        "layout": "vertical",
+                        "flex": 0,
+                        "spacing": "sm",
+                        "contents": [
+                          {
+                            "type": "button",
+                            "action": {
+                              "type": "uri",
+                              "label": "CALL",
+                              "uri": "https://linecorp.com"
+                            },
+                            "height": "sm",
+                            "style": "link"
+                          },
+                          {
+                            "type": "button",
+                            "action": {
+                              "type": "uri",
+                              "label": "WEBSITE",
+                              "uri": "https://linecorp.com"
+                            },
+                            "height": "sm",
+                            "style": "link"
+                          },
+                          {
+                            "type": "spacer",
+                            "size": "sm"
+                          }
+                        ]
+                      }
+                    };
+                    store.fetchStore().then(data => {
+                        
                         if (data == -1){
                             event.reply('找不到資料');
                         }else if(data == -9){                    
@@ -206,7 +206,7 @@ bot.on('message', function (event) {
                         }else{
                             arr.push(temp.temp_store);
                             for(var i = 0; i<data.length; i++){
-                                consloe.log(data.storeName, data.storeAdd, data.storeTel);
+                                // consloe.log(data.storeName, data.storeAdd, data.storeTel);
                                 // o.body.contents[0].text=data.storeName;
                                 // o.body.contents[1].contents[0].contents[1].text=data.storeAdd;
                                 // o.body.contents[1].contents[1].contents[1].text=data.storeTel;
