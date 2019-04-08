b13f813328162f2afd183d7357d5011002aa591a
joe890120 <joelin890120@gmail.com>
2019-04-08 18:02:30 +0800

    linebot108508

--- index.js
+++ index.js
@@ -103,13 +103,15 @@ bot.on('message', function (event) {
                                 o.body.contents[1].contents[1].contents[1].text=data[i].storeTel;
                                 arr[0].contents.contents.push(o);
                                 
-                                console.log(arr[0].contents.contents[i].body.contents[0].text);
+                                // console.log(arr[0].contents.contents[i].body.contents[0].text);
                                 // console.log("length : "+data.length+ " i : "+i+" arr: "+arr.length+" ,data: "+data[i].storeName+", "+data[i].storeAdd+", "+data[i].storeTel)
                             }
                             console.log("last-> length : "+data.length+ " i : "+i+" arr: "+arr.length)
 
                             event.reply(arr[0]);
-                            
+                            for(var i = 0; i<data.length; i++){
+                                console.log(arr[0].contents.contents[i].body.contents[0].text);
+                            }
                             arr[0].contents.contents.length=0;
                             arr.length = 0;
                             data.length = 0;
