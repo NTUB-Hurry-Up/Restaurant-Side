68f5a6fb917fdd9a952f3a3d26b004becd6d8b36
_Joe <joelin890120@gmail.com>
2019-04-12 01:40:30 +0800

    linebot108508

--- index.js
+++ index.js
@@ -236,88 +236,122 @@ bot.on('message', function (event) {
                             arr2.push(temp.temp_menu);
                             for (var i = 0; i < data.length; i++) {
                                 console.log(data[i].foodid)
-                                arr2[0].contents.contents.push({
-                                    "type": "flex",
-                                    "altText": "Flex Message",
-                                    "contents": {
-                                      "type": "bubble",
-                                      "hero": {
-                                        "type": "image",
-                                        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_2_restaurant.png",
-                                        "size": "full",
-                                        "aspectRatio": "20:13",
-                                        "aspectMode": "cover",
-                                        "action": {
-                                          "type": "uri",
-                                          "label": "Action",
-                                          "uri": "https://linecorp.com"
-                                        }
-                                      },
-                                      "body": {
-                                        "type": "box",
-                                        "layout": "vertical",
-                                        "spacing": "md",
-                                        "action": {
-                                          "type": "uri",
-                                          "label": "Action",
-                                          "uri": "https://linecorp.com"
-                                        },
-                                        "contents": [
-                                          {
+                            arr2[0].contents.contents.push({
+
+                                "type": "bubble",
+                                "hero": {
+                                    "type": "image",
+                                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_2_restaurant.png",
+                                    "size": "full",
+                                    "aspectRatio": "20:13",
+                                    "aspectMode": "cover",
+                                    "action": {
+                                        "type": "uri",
+                                        "label": "Action",
+                                        "uri": "https://linecorp.com"
+                                    }
+                                },
+                                "body": {
+                                    "type": "box",
+                                    "layout": "vertical",
+                                    "spacing": "md",
+                                    "action": {
+                                        "type": "uri",
+                                        "label": "Action",
+                                        "uri": "https://linecorp.com"
+                                    },
+                                    "contents": [
+                                        {
                                             "type": "text",
-                                            "text": data[i].foodName,
-                                            "size": "xxl",
+                                            "text": data[i].foodid,
+                                            "size": "xl",
                                             "weight": "bold"
-                                          },
-                                          {
+                                        },
+                                        {
                                             "type": "box",
                                             "layout": "vertical",
                                             "spacing": "sm",
                                             "contents": [
-                                              {
-                                                "type": "box",
-                                                "layout": "baseline",
-                                                "contents": [
-                                                  {
-                                                    "type": "text",
-                                                    "text": "NTD"+"data[i].foodPrice",
-                                                    "flex": 0,
-                                                    "margin": "sm",
-                                                    "size": "xl",
-                                                    "align": "end",
-                                                    "weight": "regular"
-                                                  }
-                                                ]
-                                              }
+                                                {
+                                                    "type": "box",
+                                                    "layout": "baseline",
+                                                    "contents": [
+                                                        {
+                                                            "type": "icon",
+                                                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/restaurant_regular_32.png"
+                                                        },
+                                                        {
+                                                            "type": "text",
+                                                            "text": data[i].foodPrice,
+                                                            "flex": 0,
+                                                            "margin": "sm",
+                                                            "weight": "bold"
+                                                        },
+                                                        {
+                                                            "type": "text",
+                                                            "text": "400kcl",
+                                                            "size": "sm",
+                                                            "align": "end",
+                                                            "color": "#AAAAAA"
+                                                        }
+                                                    ]
+                                                },
+                                                {
+                                                    "type": "box",
+                                                    "layout": "baseline",
+                                                    "contents": [
+                                                        {
+                                                            "type": "icon",
+                                                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/restaurant_large_32.png"
+                                                        },
+                                                        {
+                                                            "type": "text",
+                                                            "text": data[i].foodName,
+                                                            "flex": 0,
+                                                            "margin": "sm",
+                                                            "weight": "bold"
+                                                        },
+                                                        {
+                                                            "type": "text",
+                                                            "text": "550kcl",
+                                                            "size": "sm",
+                                                            "align": "end",
+                                                            "color": "#AAAAAA"
+                                                        }
+                                                    ]
+                                                }
                                             ]
-                                          },
-                                          {
+                                        },
+                                        {
                                             "type": "text",
                                             "text": "Sauce, Onions, Pickles, Lettuce & Cheese",
-                                            "size": "sm",
+                                            "size": "xxs",
                                             "color": "#AAAAAA",
                                             "wrap": true
-                                          }
-                                        ]
-                                      },
-                                      "footer": {
-                                        "type": "box",
-                                        "layout": "vertical",
-                                        "contents": [
-                                          {
+                                        }
+                                    ]
+                                },
+                                "footer": {
+                                    "type": "box",
+                                    "layout": "vertical",
+                                    "contents": [
+                                        {
+                                            "type": "spacer",
+                                            "size": "xxl"
+                                        },
+                                        {
                                             "type": "button",
                                             "action": {
-                                                "type": "message",
+                                                "type": "uri",
                                                 "label": "Add to Cart",
-                                                "text": data[i].foodid
+                                                "uri": "https://linecorp.com"
                                             },
                                             "color": "#905C44",
                                             "style": "primary"
-                                          }
-                                        ]
-                                      }
-                                    }
-                                  })
+                                        }
+                                    ]
+                                }
+                            })
                             }
                             event.reply(arr2[0]);
                             arr2[0].contents.contents.length = 0;
