e43328529f3a38e12baf888ec283fda4d736a96d
_Joe <joelin890120@gmail.com>
2019-04-12 01:41:17 +0800

    linebot108508

--- index.js
+++ index.js
@@ -282,7 +282,7 @@ bot.on('message', function (event) {
                                                         },
                                                         {
                                                             "type": "text",
-                                                            "text": data[i].foodPrice,
+                                                            "text": "$10.5",
                                                             "flex": 0,
                                                             "margin": "sm",
                                                             "weight": "bold"
@@ -306,7 +306,7 @@ bot.on('message', function (event) {
                                                         },
                                                         {
                                                             "type": "text",
-                                                            "text": data[i].foodName,
+                                                            "text": "$15.5",
                                                             "flex": 0,
                                                             "margin": "sm",
                                                             "weight": "bold"
