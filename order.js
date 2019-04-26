4d89b55c62e96c7722d0f46a0425b94b8b51f94f
joelin890120 <ntub108508@gmail.com>
2019-04-26 19:25:01 +0800

    108508

--- order.js
+++ order.js
@@ -23,21 +23,17 @@ var fetchOrder = async function(storeId, msg3){
     //回傳執行結果
     return result;  
 }
-//
+//未接受訂單改為接受
 var AccpetOrder = async function(msg3){
     //存放結果
     let result;  
-
     //讀取資料庫
-    await query('UPDATE	"order"	SET	status="已接受未製作"	WHERE	status="未接受" AND	orderid=$1;', [msg3])
+    await query('UPDATE	 order	SET	"status"="已接受未製作"	WHERE	"status"="未接受" AND	"orderid"=$1;', [msg3])
     .then((data) => { 
         result = data.rowCount;  //回傳資料數 
     }, (error) => {
         result = -9;  //執行錯誤
     });
-
-
-    //回傳執行結果
     return result;  
 }
 
