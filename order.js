f2a03f15e50b086746b2446d62de241c1648a936
unknown <ntub108508@gmail.com>
2019-04-30 23:35:39 +0800

    108508

--- order.js
+++ order.js
@@ -29,7 +29,7 @@ var acceptOrder = async function(storeid,msg3){
     //存放結果
     let result;  
     //讀取資料庫
-    await query('UPDATE	order SET status=$4 WHERE status=$3 AND storeid=$1  AND orderid=$2',[storeid,msg3,'未接單','已接單未製作'])
+    await query('UPDATE	"order"	SET	status=$4	WHERE	storeid=$1	AND	orderid=$2	AND	status=$3;',[storeid,msg3,'未接單','已接單未製作'])
     .then((data) => { 
         result = data.rowCount;  //回傳資料數 
     }, (error) => {
