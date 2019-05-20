aa13b3fa20944287497eeeffd0141eeea8dae99f
ntub108508 <ntub108508@gmail.com>
2019-05-20 16:49:45 +0800

    108508

--- index.js
+++ index.js
@@ -110,6 +110,7 @@ bot.on('message', function (event) {
                         }
 
                     })
+                    event.reply(arr)
                 } else if (msg2 == "接受訂單") {
                     order.acceptOrder(storeid, msg3).then(data => {
                         if (data == -9) event.reply('執行錯誤');
