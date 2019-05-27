6314e3bd6966698779b94454c9c1a19af8575f25
unknown <ntub108508@gmail.com>
2019-05-27 19:36:39 +0800

    108508

--- index.js
+++ index.js
@@ -124,12 +124,12 @@ bot.on('message', function (event) {
                                     arr[0].contents.contents[i].body.contents[3].contents[1].text = data[i].name;
                                     arr[0].contents.contents[i].body.contents[4].contents[1].text = data[i].phone;
                                 }
-                                for (var j = 0; j < data.length; j++) {
-                                    arr[0].contents.contents[i].body.contents[j + 7] = lodash.cloneDeep(temp.temp_acceptOrder_detail_repeat)
-                                    arr[0].contents.contents[i].body.contents[j + 7].contents[0].text = data[i].foodName;
-                                    arr[0].contents.contents[i].body.contents[j + 7].contents[1].text = data[i].amount;
-                                    arr[0].contents.contents[i].body.contents[j + 7].contents[2].text = data[i].price;
-                                }
+                                // for (var j = 0; j < data.length; j++) {
+                                //     arr[0].contents.contents[i].body.contents[j + 7] = lodash.cloneDeep(temp.temp_acceptOrder_detail_repeat)
+                                //     arr[0].contents.contents[i].body.contents[j + 7].contents[0].text = data[i].foodName;
+                                //     arr[0].contents.contents[i].body.contents[j + 7].contents[1].text = data[i].amount;
+                                //     arr[0].contents.contents[i].body.contents[j + 7].contents[2].text = data[i].price;
+                                // }
                             }
                             event.reply(arr)
                         }
