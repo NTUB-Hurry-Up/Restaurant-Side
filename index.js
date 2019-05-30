bc1b9121dd9538acf052de7197d88073064b44b7
ntub108508 <ntub108508@gmail.com>
2019-05-30 20:23:22 +0800

    108508

--- index.js
+++ index.js
@@ -261,7 +261,7 @@ bot.on('message', function (event) {
                 } else if(msg2 =="完成"){
                     order.completedOrder(storeid, msg3).then(data => {
                         if (data == -9) event.reply('執行錯誤');
-                        else event.reply('已接單');
+                        else event.reply('完成');
                     })
                 }
             } else if (msg1 == "店家資訊") {
